3a1ba096ae079ac1bdd89809dd69d3ba42adcccf
Saif el-dein tarek <saifeldeintarek@gmail.com>
2026-09-24 17:26:46 +0300

    validate request: create register and login validate

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool {
+        return true;
+    }
+
+    public function rules(): array {
+        return [
+            'username' => ['required', 'string', 'unique:users,username'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
+            'role' => ['nullable', 'in:Super Admin,Project Member'],
+        ];
+    }
+}

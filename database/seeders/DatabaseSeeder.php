ac4cf9c9b09df2cd35354419a85fbbb7744e32ea
Saif el-dein tarek <saifeldeintarek@gmail.com>
2026-09-26 15:11:54 +0300

    database: create role and permission seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     public function run(): void {
-        $this->call([ 
+        $this->call([
+            PermissionSeeder::class,
             RoleSeeder::class,
         ]);
     }

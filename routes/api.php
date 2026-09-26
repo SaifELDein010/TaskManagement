165e0d61127a33bd7bc70f6101b9379fd0e62753
Saif el-dein tarek <saifeldeintarek@gmail.com>
2026-09-26 15:16:44 +0300

    route: create api CRUD of roles and permission

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -10,4 +13,8 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::get('/auth/me', [AuthController::class, 'me']);
     Route::patch('/auth/password', [AuthController::class, 'updatePassword']);
+    
+    Route::apiResource('roles', RoleController::class);
+
+    Route::put('/roles/{role}/permissions', [RoleController::class, 'syncPermissions']);
 });

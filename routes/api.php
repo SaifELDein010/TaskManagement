7d3062a272bff88e17eea8f68d41234d759ea601
Saif el-dein tarek <saifeldeintarek@gmail.com>
2026-09-26 14:07:48 +0300

    route: create api route of me and update password

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::patch('/auth/password', [AuthController::class, 'updatePassword']);
+});
